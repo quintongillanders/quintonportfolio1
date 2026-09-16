ec00e5515c006631ffcdf6c878fed8804c13242b
quintongillanders <142285437+quintongillanders@users.noreply.github.com>
2026-09-16 13:10:08 +1200

    removed november 7 launch date for quinelo

--- src/App.jsx
+++ src/App.jsx
@@ -816,7 +816,7 @@ for (const key in knowledgeBase) {
 
       <div className="card-content">
 
-        <h3>quinelo (November 7, 2026)</h3>
+        <h3>quinelo (Release TBA)</h3>
 
         <p>
           An education tool designed to help teachers save time by generating
